34afb9cfe6badd9495f28d87f7ee02b7486d482f
Axel Urizar <axelurizarmartin@gmail.com>
2024-10-19 09:36:24 +0200

    Config changes to ensure proper functioning.
      - Changed undo dir (only affects UNIX).
      - Commented "vbserver" from mason.lua ensure installed.

--- lua/config/core/set.lua
+++ lua/config/core/set.lua
@@ -11,7 +11,10 @@ vim.opt.wrap = true
 
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+
+-- Just on linux
+-- vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+
 vim.opt.undofile = true
 
 vim.opt.hlsearch = false
