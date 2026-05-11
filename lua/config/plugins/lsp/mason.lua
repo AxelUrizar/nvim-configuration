977b8ff8ec6f441e72d45df43366fe77446f92c5
Axel Urizar <me@axelurizar.dev>
2026-05-11 22:32:24 +0200

    ts_ls and ts_standard removed from mason 'ensure_installed'

--- lua/config/plugins/lsp/mason.lua
+++ lua/config/plugins/lsp/mason.lua
@@ -27,8 +27,6 @@ return {
     mason_lspconfig.setup({
       -- list of servers for mason to install
       ensure_installed = {
-        "ts_ls",
-        "ts-standard", -- ts-standard server for linting
         "html",
         "cssls",
         "tailwindcss",
